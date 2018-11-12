3d4f18e2f08927f84055888364d995872ebb38c4
Rahul Thomas <rahmtho90@gmail.com>
2018-11-11 19:26:38 -0800

    #avmxf - added max links crawled to octopus doc

--- octopus/doc.go
+++ octopus/doc.go
@@ -7,12 +7,13 @@ customize the behaviour of the crawler.
 Features
 
 Current Features of the crawler include:
-	1. Depth Limited Crawling
+	1. User specifiable Depth Limited Crawling
 	2. User specified valid protocols
 	3. User buildable adapters that the crawler feeds output to.
 	4. Filter Duplicates.
 	5. Filter URLs that fail a HEAD request.
 	6. User specifiable max timeout between two successive url requests.
+	7. User specifiable Max Number of Links to be crawled.
 
 
 Pipeline Overview
